f9e8d68fd377a163cce2370421ab4f2188a94019
Thayrone Lião da Silva <76060404+Thayrone-L@users.noreply.github.com>
2021-06-07 09:00:49 -0300

    inclusão dos cards e banner

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-      $(this).toggleClass('open');
+  $('#myModal').modal('show');
+  $('#nav-icon1').click(function(){
+    $(this).toggleClass('open');
+  });
+    $('#maior').click(function(){
+      $('#myModal').modal('hide');
     });
   });
